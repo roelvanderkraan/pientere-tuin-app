59d25a37d025307bc998b117b18adb459138120e
Roel <roel@gethippo.app>
2023-08-14 18:26:27 +0200

    Adds refresh on scene becoming active. Adds some settings to add background refresh later

--- Pientere Tuin/Pientere_TuinApp.swift
+++ Pientere Tuin/Pientere_TuinApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import CoreData
+import BackgroundTasks
 
 @main
 struct Pientere_TuinApp: App {
+    @Environment(\.scenePhase) private var phase
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
@@ -16,5 +19,34 @@ struct Pientere_TuinApp: App {
             ContentView(garden: GardenStore.getGarden(in: persistenceController.container.viewContext))
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            //case .background: scheduleAppRefresh()
+            case .active:
+                Task {
+                    await refreshData()
+                }
+            default: break
+            }
+        }
+        
+    }
+    
+    /// Refreshes data from API in background thread
+    /// Should run +/- every hour, since data in the API is only updated hourly
+    private func refreshData() async {
+        let apiHandler = ApiHandler()
+        let viewContext = persistenceController.container.newBackgroundContext()
+        viewContext.automaticallyMergesChangesFromParent = true
+        viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        let garden = GardenStore.getGarden(in: viewContext)
+        try? await apiHandler.updateTuinData(context: viewContext, garden: garden)
+    }
+    
+    /// Schedules background refresh of data, every 15 minutes
+    func scheduleAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: "studio.skipper.Pientere-Tuin.refresh")
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
+        try? BGTaskScheduler.shared.submit(request)
     }
 }
